be8be3cad336ccb4b63253924d1d69a2d2ad590c
antoninodangelo11 <reapercsp@gmail.com>
2023-04-21 10:47:43 +0200

    correction

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -4,23 +4,42 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-let parola = prompt('Inserisci parola:');
-let carattere;
-let parolaInversa = '';
+/*Con ciclo while*/
 
-let i = parola.length - 1;
+// let userWord = prompt('Inserisci parola:');
+// let reversedWord = '';
 
-while (i >= 0) {
-  carattere = parola[i];
-  parolaInversa += carattere;
-  console.log(carattere);
-  i--;
-}
+// let i = userWord.length - 1;
 
-console.log(parolaInversa);
+// while (i >= 0) {
+//   reversedWord += userWord[i];
+//   i--;
+// }
 
-if (parola == parolaInversa) {
-  console.log('La parola è palindroma');
+/*------------------------------------------------*/
+
+/* Con ciclo for e function*/
+
+let userWord = prompt('Inserisci parola:').toLowerCase();
+let reversedWord = '';
+
+if (isPalindrom(userWord)) {
+  console.log('La parola è Palindroma');
 } else {
-  console.log('La parola non è palindroma');
+  console.log('La parole non è Palindroma');
 }
+
+function isPalindrom(word) {
+  let reversedWord = '';
+  
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word[i];
+  }
+
+  if (userWord == reversedWord) {
+    return true;
+  } else {
+    return false;
+  } 
+}
+
--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -7,33 +7,40 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-function isEven(result) {
-    if (result % 2 == 0) {
-        return true;
-    } else if (result % 2 == 1) {
-        return false;
-    }
+/*Variabili ed esecuzione programma*/
+
+const divisibilty = prompt('Scegli Pari o Dispari').toLowerCase();
+const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
+/*Qui richiamiamo la funzione (getRndInteger) per generare il numero random*/
+const computerNumber = getRndInteger (1, 5);
+const sum = computerNumber + userNumber;
+
+console.log('Pari o Dispari: ', divisibilty);
+console.log('Numero PC: ', computerNumber);
+console.log('Numero Utente: ', userNumber);
+console.log('Somma: ', sum);
+
+/*Qui richiamiamo la prima funzione (evenOrOdd per capire se è pari o dispari*/
+if (divisibilty == evenOrOdd(sum)) {
+    console.log('Hai vinto :)');
+} else {
+    console.log('Hai perso :(');
 }
 
+
+/* Definizione funzioni*/
+
+/*Questa funzione (evenOrOdd) per stabilire pari o dispari*/
+function evenOrOdd(result) {
+    if (result % 2 == 0) {
+        return 'pari';
+    } 
+    return 'dispari';
+}
+
+/*Questa funzione (getRndInteger) è per il numero random del computer*/
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
-// Variabili
-const oddOrEven = prompt('Scegli Pari o Dispari').toLocaleLowerCase();
-const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
-const computerNumber = getRndInteger (1, 5);
-let sum = (computerNumber + userNumber);
-
-console.log('Numero PC: ', computerNumber);
-console.log('Numero Utente: ', userNumber);
-
-if (isEven(sum) == true && oddOrEven == 'Pari' || isEven(sum) == false && oddOrEven == 'Dispari') {
-    console.log('Hai vinto');
-} else {
-    console.log('Hai perso');
-}
-
-console.log('Somma: ', sum);
 
 
